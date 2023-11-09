d904136b0ea6063a7b3b522ff1ad530bc735ff43
kchs0529 <kchs0529@gmail.com>
2023-11-10 00:00:20 +0900

    앱 데이터 저장,조회,삭제 문제 해결

--- fridgeApp/App.js
+++ fridgeApp/App.js
@@ -7,10 +7,14 @@ export default function App() {
     <View style={styles.container}>
       <WebView
         source={{ uri: 'http://192.168.55.179:3001' }}
+        // Mixed content(보안되지 않은 HTTP 요소)를 항상 허용합니다.
         mixedContentMode="always" 
+        // JavaScript 실행을 허용합니다.
         javaScriptEnabled={true} 
+        // DOM 저장소를 활성화합니다.
         domStorageEnabled={true} 
-        originWhitelist={['*']} 
+        // 모든 원본을 허용하는 원본 허용 목록을 설정합니다.
+        originWhitelist={['http://localhost:3000', 'http://192.168.55.179:3001']} 
       />
       <StatusBar style="auto" />
     </View>
